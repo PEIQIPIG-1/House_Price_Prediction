048e2bc7bcb95e61420069fee4471ecf0fdb8a49
21012651 <112736619+21012651@users.noreply.github.com>
2023-08-06 23:03:12 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
+var map
 function initMap(){
     var options = {
         zoom: 8,
-        center: {lat: -36.848461, lng:174.763336}
+        center: {lat: 39.916668, lng:116.383331},
+        mapId: '281418b42608306f'
     };
 
-    var map = new google.maps.Map(document.getElementById('map'),options);
-    
-    // var marker = new google.maps.Marker({
-    //     position:{lat:42.4668, lng:-70.9495},
-    //     map:map,
-    //     icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-    // });
-    
-    // var infoWindow = new google.maps.InfoWindow({
-    //     content:'<h1>Lynn Ma</h1>'
-    // });
-    // marker.addListener('click',function(){
-    //     infoWindow.open(map,marker);
-    // });
+    map = new google.maps.Map(document.getElementById('map'),options);
 
     addMarker({
-        coords:{lat: -36.848461, lng:174.763336},
+        coords:{lat: 39.916668, lng:116.383331},
         iconImage:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-        content:'<h1>Auckland</h1>'
+        content:'<h1>Beijing</h1>'
     });
     addMarker({
-        coords:{lat:-36.7266638 , lng:174.6999972}
+        coords:{lat:40.37636 , lng:115.22403},
+        content:'<h1>Zhuolu</h1>'
     });
     //add marker
     function addMarker(properties){
@@ -39,15 +29,16 @@ function initMap(){
         }
 
         if(properties.content){
-            var infoWindow = new google.maps.InfoWindow({
+            var inforWindow = new google.maps.InfoWindow({
                 content:properties.content
             });
             marker.addListener('click',function(){
-                infoWindow.open(map,marker);
+                inforWindow.open(map,marker);
             });
         }
     }
 
+    //find position of user
     var infoWindow = new google.maps.InfoWindow();
 
     const locationButton = document.createElement("button");
@@ -79,7 +70,35 @@ function initMap(){
         handleLocationError(false, infoWindow, map.getCenter());
         }
     });
+
+    
+ 
+    //load json  这种方法用于请求jsonp
+    // var script = document.createElement('script');
+    // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';    
+    // document.getElementsByTagName('head')[0].appendChild(script);
+
+    //这种方法用于请求跨域资源共享(CROS) 这一行不需要eqfeed_callback()   谷歌 开发者工具 网络 response header Access-Control-Allow-Origin:*  说明允许请求
+    //也可用于加载本地资源
+    map.data.loadGeoJson('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson');   
+    
+    
+    
+    
 }
+// const eqfeed_callback = function (results) {
+//     for (let i = 0; i < results.features.length; i++) {
+//         const coords = results.features[i].geometry.coordinates;
+//         const latLng = new google.maps.LatLng(coords[1], coords[0]);
+        
+//         //document.getElementById('test').innerHTML = coords[1];
+//         new google.maps.Marker({
+//             position: latLng,
+//             map: map,
+//           });
+//     }
+// };
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
@@ -91,4 +110,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 }
 
+
+
 initMap();
+window.eqfeed_callback = eqfeed_callback;
